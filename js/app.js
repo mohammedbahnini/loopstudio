c55bb01bf342458a5098234ab3572bae70409a2d
mohammedbahnini <63128845+mohammedbahnini@users.noreply.github.com>
2022-04-18 00:07:52 +0000

    first commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const nav_menu = document.querySelector('.nav-menu');
+nav_menu.addEventListener('click' , ()=>{
+    const menu = document.querySelector('header nav');
+    const body =document.querySelector('body');
+
+    if( nav_menu.classList.contains('open')){
+        nav_menu.classList.remove('open');
+        menu.classList.remove('open');
+        body.style.overflow = 'auto';
+        body.style.height = 'auto';
+    }
+    else{
+        nav_menu.classList.add('open');
+        menu.classList.add('open');
+        body.style.overflow = 'hidden';
+        body.style.height = '100vh';
+    }
+});
